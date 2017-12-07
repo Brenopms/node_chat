2f7a4636635380585827f891999cb3bd934ade53
Breno Silva <brenosilva1@gmail.com>
2017-12-06 23:45:06 -0200

    structuring and modularizing the router

--- server.js
+++ server.js
@@ -2,19 +2,13 @@
 
 const express = require('express');
 const app = express();
+const chat = require('./app');
 
 app.set('port', process.env.PORT || 3000)
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
-app.get('/' , (req, res, next) => {
-	res.render('login');
-});
-
-
-app.get('/dashboard', (req, res, next) => {
-	res.send('<h1>This is the dashboard page! Middleware says: ' + req.hello + '</h1>');
-});
+app.use('/', chat.router);
 
 app.listen(app.get('port'), () => {
 	console.log('Chat running on port:', app.get('port'));
