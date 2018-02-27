8c759775379273e36531bf4d1ff228375f99773d
Breno Silva <brenosilva1@gmail.com>
2018-02-27 12:47:31 -0300

    serialize and deserialize user

--- app/auth/index.js
+++ app/auth/index.js
@@ -5,6 +5,17 @@ const h = require('../helpers');
 const FacebookStrategy = require('passport-facebook').Strategy;
 
 module.exports = () => {
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        h.findById(id)
+            .then(user => done(null, user))
+            .catch(error => console.log('Error when deserializing the user'));
+    });
+
     let authProcessor =  (accessToken, refreshToken, profile, done) => {
         //Find a user in the local db using profile.id
         h.findOne(profile.id)
