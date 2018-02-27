8ff7ef25922db0e62bca99d919891e86fd8a6927
Breno Silva <brenosilva1@gmail.com>
2018-02-26 22:59:06 -0300

    authProcessor

--- app/auth/index.js
+++ app/auth/index.js
@@ -1,13 +1,25 @@
 'use strict';
 const passport = require('passport');
 const config = require('../config');
+const h = require('../helpers');
 const FacebookStrategy = require('passport-facebook').Strategy;
 
 module.exports = () => {
     let authProcessor =  (accessToken, refreshToken, profile, done) => {
         //Find a user in the local db using profile.id
-        //if the user if found, return the user data using done()
-        //if the user is not found, create one in the local db and return
+        h.findOne(profile.id)
+            .then((result) => {
+                //if the user if found, return the user data using done()
+                if(result){
+                    done(null, result);
+                } else {
+                    //if the user is not found, create one in the local db and return
+                    //Create a new user and return
+                    h.createNewUser(profile)
+                        .then(newChatUser => done(null, newChatUser))
+                        .catch(error => console.log('Error when creating new user'))
+                }
+            })
     };
     passport.use(new FacebookStrategy(config.fb, authProcessor));
 }
