707c730ae618fe42f8c5d1205faf55b78278b199
Breno Silva <brenosilva1@gmail.com>
2018-02-26 22:57:58 -0300

    add user model

--- app/db/index.js
+++ app/db/index.js
@@ -2,10 +2,22 @@
 const config = require('../config');
 const Mongoose = require('mongoose').connect(config.dbURI);
 
+//Log an error if the connection fails
 Mongoose.connection.on('error', error => {
     console.log('Mongodb error', error);
 });
 
+//Create a Schema that defines the structure for storing user
+const chatUser = new Mongoose.Schema({
+    profileId: String,
+    fullName: String,
+    profilePic: String
+});
+
+//Turn the schema into a usable model
+let userModel = Mongoose.model('chatUser', chatUser);
+
 module.exports = {
-    Mongoose
+    Mongoose,
+    userModel
 }
