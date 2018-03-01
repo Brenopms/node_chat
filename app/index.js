c7406f6e7c00e5c5ef1737ec68d605aab0b51a15
Breno Silva <brenosilva1@gmail.com>
2018-02-28 23:03:56 -0300

    bind socket.io to server

--- app/index.js
+++ app/index.js
@@ -3,7 +3,17 @@
 //Social Authentication Logic
 require('./auth')();
 
+//Create an IO Server instance
+
+let ioServer = app => {
+	const server = require('http').Server(app);
+	const io = require('socket.io')(server);
+	require('./socket')(io);
+	return server;
+}
+
 module.exports = {
 	router: require('./router')(),
-	session: require('./sessions')
+	session: require('./sessions'),
+	ioServer
 }
