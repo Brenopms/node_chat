80a514d49bdc5dff780f1f0e696d54417f934587
Breno Silva <brenosilva1@gmail.com>
2018-03-06 22:14:06 -0300

    rendering the chatroom list

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,10 +1,25 @@
 'use strict';
 
-
 module.exports = (io, app) => {
     let allrooms = app.locals.chatrooms;
 
+    allrooms.push({
+        room: 'Good Food',
+        roomID: '0001',
+        users: []
+    });
+
+    allrooms.push({
+        room: 'Cloud Computing',
+        roomID: '0002',
+        users: []
+    });
+
     io.of('/roomslist').on('connection', socket => {
         console.log('Socket.io connected to the client');
+        socket.on('getChatrooms', () => {
+            console.log('nice');
+            socket.emit('chatRoomsList', JSON.stringify(allrooms));
+        });
     });
 }
